83b88a52857a925fa9b0ecafc32d4feaf00bbabb
RodrigoSoldi <rodrigosoldi@icloud.com>
2015-08-28 10:01:05 -0300

    ïnitial commit

---
+++ MyStage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyStage
+//
+//  Created by Rodrigo Soldi Lopes on 28/08/15.
+//  Copyright (c) 2015 DreamTeamIOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
