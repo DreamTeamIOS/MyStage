2d41922a3903ca2908241f15f343a492df684823
RodrigoSoldi <rodrigosoldi@icloud.com>
2015-08-28 10:28:23 -0300

    Added AFNetworking

---
+++ MyStage/HeaderObjC.h
@@ -0,0 +1,14 @@
+//
+//  HeaderObjC.h
+//  MyStage
+//
+//  Created by Rodrigo Soldi Lopes on 28/08/15.
+//  Copyright (c) 2015 DreamTeamIOS. All rights reserved.
+//
+
+#ifndef MyStage_HeaderObjC_h
+#define MyStage_HeaderObjC_h
+
+#import "AFNetworking/AFNetworking.h"
+
+#endif
